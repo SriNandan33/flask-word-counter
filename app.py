62835f5f27509f291119d0941c941b7ba7b427a6
= <=>
2018-10-17 10:53:37 +0530

    basic version without DB and Queue

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask
-
+import requests
+from flask import Flask, render_template, redirect, url_for, request
+from forms import UrlInputForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f97d8be751f31b5f16f143bb9fef833e'
 
 
-@app.route('/')
+
+def count_words_at_url(url):
+	resp = requests.get(url)
+	return len(resp.text.split())
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	return '<h1>Edyst Challenge</h1>'
+	result = None
+	form = UrlInputForm(request.form)
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			word_count = count_words_at_url(form.url.data)
+			result = word_count
+		else:
+			print('form is invalid')
 
+	return render_template('home.html', form=form, result=result)
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from flask_wtf.html5 import URLField
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, url
+
+
+class UrlInputForm(FlaskForm):
+	url = URLField('url', validators=[DataRequired(), url() ])
+	submit = SubmitField('submit')
